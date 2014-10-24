43f5978abf604b84aa80298bf129b32700e5fa82
Kenneth Dombrowski <kenneth@ylayali.net>
2014-10-24 14:58:31 -0400

    load all sections from all configs into menu; give default values to config parser

--- data-in-the-mines/ditm/config.py
+++ data-in-the-mines/ditm/config.py
@@ -1,9 +1,24 @@
 
 from collections import defaultdict
 import ConfigParser
+import itertools
 import os
 
 REPORT_SECTION = 'ditm-report'
+
+# Periodic reports are configured via a series of config dirs corresponding to:
+PERIODS = ['hourly', 'daily', 'weekly', 'monthly', 'yearly', 'examples']
+
+def configured_cp():
+    """sets default values for optional config file sections"""
+    return ConfigParser.SafeConfigParser({
+        'section': 'my reports',
+        'title': 'My Report',
+        'template': 'data-in-the-mines-default-report.html',
+        'source': '',
+        'handler': '',
+    })
+
 
 def read_configs(config_dir):
     """parse directory of config files, return dict
@@ -17,18 +32,27 @@ def read_configs(config_dir):
     >>> projroot = up(up(up(os.path.realpath(__file__))))
     >>> confroot = os.path.sep.join([projroot, 'etc', 'data-in-the-mines', 'examples.d'])
     >>> c = read_configs(confroot)
+    >>> # example.d configs reference 2 sources for 4 reports
     >>> len(c)
-    1
+    2
+    >>> # 3 reports are configured to receive input from mtcars.input_generator
     >>> ig = 'data-in-the-mines.examples.mtcars.input_generator'
     >>> len(c[ig])
     3
     >>> report_ids = sorted([ conf['report_id'] for conf in c[ig] ])
     >>> report_ids
     ['mtcars_kmeans3', 'mtcars_kmeans4', 'mtcars_stats']
+    >>> # get names of all reports across all periods
+    >>> global_confs = [ confs for (src, confs) in c.items() ]
+    >>> global_confs = [ i for i in itertools.chain.from_iterable(global_confs) ]
+    >>> report_ids = sorted([ conf['report_id'] for conf in global_confs ])
+    >>> report_ids
+    ['mtcars_kmeans3', 'mtcars_kmeans4', 'mtcars_stats', 'visual_playlists']
     """
-    cp = ConfigParser.SafeConfigParser()
     configs = defaultdict(list)
+    config_dir = os.path.realpath(os.path.expanduser(config_dir))
     for f in os.listdir(config_dir):
+        cp = configured_cp()
         if f[-5:] == ".conf":
             report_id = f[:-5]
             file_path = os.path.sep.join([config_dir, f])
@@ -43,3 +67,27 @@ def read_configs(config_dir):
             })
     return dict(configs)
 
+
+def sections_from_configs(config_root):
+    """parse all configs and return all sections defined
+
+    >>> up = lambda x: os.path.dirname(x)
+    >>> projroot = up(up(up(os.path.realpath(__file__))))
+    >>> confroot = os.path.sep.join([projroot, 'etc', 'data-in-the-mines'])
+    >>> s = sections_from_configs(confroot)
+    >>> s
+    ['segments', 'stats', 'visuals']
+    """
+    cp = configured_cp()
+    sections = set()
+    config_root = os.path.realpath(os.path.expanduser(config_root))
+    for p in PERIODS:
+        config_dir = os.path.sep.join([config_root, '{}.d'.format(p)])
+        for f in os.listdir(config_dir):
+            if f[-5:] == ".conf":
+                file_path = os.path.sep.join([config_dir, f])
+                cp.read(file_path)
+                section = cp.get(REPORT_SECTION, 'section')
+                if section:
+                    sections.add(section)
+    return sorted(list(sections))
--- data-in-the-mines/ditm/flask/blueprint.py
+++ data-in-the-mines/ditm/flask/blueprint.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 
 from flask import Blueprint, abort, current_app, redirect, render_template, \
     send_from_directory
@@ -7,15 +8,12 @@ from jinja2 import TemplateNotFound
 from werkzeug.utils import secure_filename
 
 from ditm import reports
-from ditm.config import read_configs
+from ditm.config import PERIODS, read_configs, sections_from_configs
 
 bp_dir = os.path.dirname(os.path.realpath(__file__))
 tmpl_dir = os.path.sep.join([bp_dir, 'templates'])
 ditm = Blueprint('data-in-the-mines', __name__, template_folder=tmpl_dir,
     static_folder='static')
-
-# Periodic reports are configured via a series of config dirs corresponding to:
-PERIODS = ['hourly', 'daily', 'weekly', 'monthly', 'yearly', 'examples']
 
 @ditm.route('/')
 @ditm.route('/<section>')
@@ -26,6 +24,7 @@ PERIODS = ['hourly', 'daily', 'weekly', 'monthly', 'yearly', 'examples']
 def show(section='index', period='examples', report_id='', report_date='', filename=''):
 
     if period not in PERIODS:
+        current_app.logger.error('bad period: {}'.format(period))
         abort(404)
 
     ditm_path = current_app.config['DITM_URL_PREFIX']
@@ -36,66 +35,77 @@ def show(section='index', period='examples', report_id='', report_date='', filen
     config_root = current_app.config['DITM_CONFIG_ROOT']
     config_root = os.path.realpath(os.path.expanduser(config_root))
     config_dir = os.path.sep.join([ config_root, '{}.d'.format(period) ])
+
     configs = read_configs(config_dir)
+    sections = sections_from_configs(config_root)
 
     # user must select, in order:
     # section [user/config defined]
     # period [daily, weekly, examples, ...] corresponding to .d config dirs
     # report_id -> most recent report is displayed w/menu of past dates
-    sections = set()
     report_ids = set()
     report_template = ''
     for (src, src_configs) in configs.iteritems():
         for c in src_configs:
-            sections.add(c['section'])
             if section == c['section']:
                 report_ids.add(c['report_id'])
                 if c['report_id'] == report_id:
                     report_template = c.get('template', '')
-    sections = sorted(list(sections))
     report_ids = sorted(list(report_ids))
 
     if section not in sections + ['index']:
+        current_app.logger.error('bad section: {} {}'.format(section, sections))
         abort(404)
 
-    if report_id:
-        if report_id not in report_ids:
-            abort(404)
-        report_dir = os.path.sep.join([report_root, section, period, report_id])
-        report_dates = reports.existing_reports(report_dir)
-    else:
-        report_dates = []
+    last_date = ''
+    report_dates = []
+    # having no reports configured for a time period is not an error
+    if report_ids:
+        if not report_id:
+            report_id = report_ids[0]
 
-    try:
-        last_date = report_dates[-1]
-    except IndexError:
-        last_date = ''
-
-    if not report_date:
-        report_date = last_date
-        # the only condition we have a report_id but no report_date is when
-        # user has just selected a report_id; send them to most recent
         if report_id:
-            report_path = "/".join([
-                ditm_path,
+            if report_id not in report_ids:
+                current_app.logger.error('bad report: {} {}'.format(report_id, report_ids))
+                abort(404)
+            report_dir = os.path.sep.join([report_root, section, period, report_id])
+            report_dates = reports.existing_reports(report_dir)
+
+        try:
+            last_date = report_dates[-1]
+        except IndexError:
+            pass
+
+        if last_date and not report_date:
+            report_date = last_date
+            # the only condition we have a report_id but no report_date is when
+            # user has just selected a report_id; send them to most recent
+            if report_id:
+                report_path = "/".join([
+                    ditm_path,
+                    section,
+                    period,
+                    report_id,
+                    report_date
+                ]).rstrip("/")
+                current_app.logger.info('redirecting to {}'.format(report_path))
+                return redirect(report_path)
+
+        # if we have a filename, request is for page resource
+        if filename:
+            filename = secure_filename(filename)
+            resource_dir = os.path.sep.join([
+                report_root,
                 section,
                 period,
                 report_id,
-                report_date
+                report_date,
             ])
-            return redirect(report_path)
+            current_app.logger.info('serving {}/{}'.format(resource_dir, filename))
+            return send_from_directory(resource_dir, filename)
 
-    # if we have a filename, request is for page resource
-    if filename:
-        filename = secure_filename(filename)
-        resource_dir = os.path.sep.join([
-            report_root,
-            section,
-            period,
-            report_id,
-            report_date,
-        ])
-        return send_from_directory(resource_dir, filename)
+    if not report_id:
+        report_template = 'data-in-the-mines-nothing-there.html'
 
     # else returning page itself
     tmpl_kwargs = {
@@ -114,4 +124,5 @@ def show(section='index', period='examples', report_id='', report_date='', filen
     try:
         return render_template('data-in-the-mines.html', **tmpl_kwargs)
     except TemplateNotFound:
+        current_app.logger.error('data-in-the-mines.html not found')
         abort(404)
